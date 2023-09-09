bcc89416a8417de7886a4afd3d1efa905ec016c8
ilham Suryana <szuryanailham090102@gmail.com>
2023-09-09 10:53:51 +0700

    menyelesaikan halaman check bserta modal record

--- resources/js/Components/CheckPage_Items/MenuCheck.jsx
+++ resources/js/Components/CheckPage_Items/MenuCheck.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo1 from "@/images/Stethoscope.png";
 import logo2 from "@/images/Health checkup.png";
+import ModalRecordUser from "./ModalRecordUser";
 function MenuCheck() {
     const scrollToTarget = () => {
         const targetElement = document.getElementById("targetElementId");
@@ -10,6 +11,7 @@ function MenuCheck() {
     };
     return (
         <>
+            <ModalRecordUser />
             <h1 className="text-lg font-bold p-2 text-center">About Us</h1>
             <div className="w-full flex justify-evenly h-auto p-5">
                 {/* title */}
@@ -23,7 +25,7 @@ function MenuCheck() {
                 <button
                     className="btn btn-square"
                     onClick={() =>
-                        document.getElementById("recordModal").showModal()
+                        document.getElementById("record_modal").showModal()
                     }
                 >
                     <img src={logo2} alt="logo2" />
--- resources/js/Components/CheckPage_Items/ModalRecordUser.jsx
+++ resources/js/Components/CheckPage_Items/ModalRecordUser.jsx
@@ -3,16 +3,73 @@ import React from "react";
 function ModalRecordUser() {
     return (
         <>
-            <dialog id="my_modal_2" className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">
-                        Press ESC key or click outside to close
-                    </p>
+            <dialog id="record_modal" className="modal">
+                <form action=""></form>
+                <div className="modal-box w-11/12 max-w-5xl">
+                    {/*  menu record by date ..... */}
+                    <ul className="menu menu-horizontal m-auto bg-base-200 w-auto p-2 flex justify-evenly">
+                        <li>
+                            <button className="btn-info">
+                                <a>record 1 </a>
+                            </button>
+                        </li>
+                        <li>
+                            <button className="btn-info">
+                                <a>record 2 </a>
+                            </button>
+                        </li>
+                        <li>
+                            <button className="btn-info">
+                                <a>record 3 </a>
+                            </button>
+                        </li>
+                    </ul>
+                    {/*  name of docter .......... */}
+                    <div className="mt-2">
+                        <span className="label-text">name of Docter</span>
+                        <input
+                            type="text"
+                            placeholder="dokter ..."
+                            className="input input-bordered w-full"
+                            disabled
+                        />
+                    </div>
+                    {/*  date of check  .......... */}
+                    <div className="mt-2">
+                        <span className="label-text">date of check </span>
+                        <input
+                            type="date"
+                            placeholder="dokter ....."
+                            className="input input-bordered w-full"
+                            disabled
+                        />
+                    </div>
+                    {/*  content .......... */}
+                    <div className="mt-2">
+                        <span className="label-text">date of check </span>
+                        <input
+                            type="text"
+                            placeholder="dokter ....."
+                            className="input input-bordered w-full"
+                            disabled
+                        />
+                    </div>
+                    {/* content of history record */}
+                    {/* box */}
+                    <span className="label-text mt-5">date of check </span>
+                    <textarea
+                        disabled
+                        placeholder="Bio"
+                        className="textarea textarea-bordered textarea-lg w-full "
+                    ></textarea>
+
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
                 </div>
-                <form method="dialog" className="modal-backdrop">
-                    <button>close</button>
-                </form>
             </dialog>
         </>
     );
