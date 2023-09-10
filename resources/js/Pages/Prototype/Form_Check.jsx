05187cd19b18db756662e415fc2b95b4609bddcd
ilham Suryana <szuryanailham090102@gmail.com>
2023-09-11 01:19:30 +0700

    membuat form periksa pasien

---
+++ resources/js/Pages/Prototype/Form_Check.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import NavbarForUser from "@/Components/Specialist_item/NavbarForUser";
+import Footer from "@/Components/Footer";
+function Form_Check() {
+    return (
+        <>
+            <NavbarForUser />
+            {/* judul  */}
+            <h1 className="text-lg font-bold  text-center mt-20">
+                Checking Form
+            </h1>
+            <form action="" className="p-3">
+                {/* masukan calon pasien */}
+                <label htmlFor="">
+                    <span className="label-text ">patient's name</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="name of patient's...."
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* masukan alamat  */}
+                <label htmlFor="">
+                    <span className="label-text ">patient address</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="address...."
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* tanggal periksa  */}
+                {/* otomatis disi oleh database */}
+                <label htmlFor="">
+                    <span className="label-text ">Check_Date</span>
+                </label>
+                <input
+                    type="date"
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* waktu ..... */}
+                <label htmlFor="">
+                    <span className="label-text ">time of check</span>
+                </label>
+                <input
+                    type="time"
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* nama dokter  */}
+                {/* otomatis diisi oleh database  */}
+                <label htmlFor="">
+                    <span className="label-text ">name of Docter</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Dokter Name ...."
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* status BPJS  */}
+                <div className=" w-full mt-3 flex justify-evenly  ">
+                    <div>
+                        <label className="cursor-pointer label ">
+                            <span className="label-text p-2">
+                                Health Insurance
+                            </span>
+                            <input
+                                type="checkbox"
+                                className="checkbox checkbox-info mt-3"
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label className="cursor-pointer label">
+                            <span className="label-text p-2">Normal</span>
+                            <input
+                                type="checkbox"
+                                className="checkbox checkbox-info mt-3"
+                            />
+                        </label>
+                    </div>
+                </div>
+                {/* jika mengunakan BPJS , masukan nomor BPJS */}
+                <label htmlFor="">
+                    <span className="label-text ">number of Insurence</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="number of Insurence......"
+                    className="input w-full bg-base-color mt-3"
+                />
+                {/* masukan ket keluahan  */}
+                {/* md */}
+                <textarea
+                    placeholder="complaint ..."
+                    className="textarea textarea-bordered textarea-md w-full  bg-base-color mt-3 "
+                ></textarea>
+                {/* button */}
+                <div className="w-full  b mt-5 flex ">
+                    <button
+                        type="submit"
+                        className="btn mx-auto text-white bg-[#1279FF]"
+                    >
+                        Submit
+                    </button>
+                </div>
+                {/* footer  */}
+            </form>
+            <Footer />
+        </>
+    );
+}
+
+export default Form_Check;
--- routes/proto.php
+++ routes/proto.php
@@ -15,4 +15,7 @@ Route::prefix('/proto')->group(function () {
 
      // http://127.0.0.1:8000/proto/Check_Page
      Route::inertia('/Check_Page', 'Prototype/Check_Page');
+
+         // http://127.0.0.1:8000/proto/Form_Check
+         Route::inertia('/Form_Check', 'Prototype/Form_Check');
 });
