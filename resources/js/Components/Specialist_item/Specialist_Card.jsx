474d92e78cbefbfc478253b151d3b7ffeaa145bb
ilham Suryana <szuryanailham090102@gmail.com>
2023-09-12 16:40:26 +0700

    menyelesaikan responsive page size style

--- resources/js/Components/Specialist_item/Specialist_Card.jsx
+++ resources/js/Components/Specialist_item/Specialist_Card.jsx
@@ -7,24 +7,27 @@ function Specialist_Card({ specialists }) {
         <>
             {/* container Card  */}
             <div className=" flex flex-col justify-center items-center z-0">
-                {/* judul container... */}
-                <h1 className="text-lg font-bold p-2 text-center md:text-xl">
-                    Specialist{" "}
-                </h1>
-                <div className="w-full h-auto p-3  bg-base-color z-0 md:flex md:flex-wrap md:justify-center">
-                    {/* item card specialist.....*/}
-                    {specialists.map((specialist) => (
-                        <CardItem
-                            key={specialist.id}
-                            SpecialistName={specialist.name}
-                            text={specialist.description}
-                            image={`/images/specialist${specialist.id}.png`}
-                            link={`/schedule/${specialist.id}`}
-                        />
-                    ))}
-                    {/* akhir item card specialist .....*/}
-                    {/* see more  */}
+                <div className="bg-base-color w-full h-auto mt-10">
+                    {/* judul container... */}
+                    <h1 className="text-lg font-bold p-2 text-center md:text-xl">
+                        Specialist{" "}
+                    </h1>
+                    <div className="w-full h-auto p-3   z-0 md:flex md:flex-wrap md:justify-center">
+                        {/* item card specialist.....*/}
+                        {specialists.map((specialist) => (
+                            <CardItem
+                                key={specialist.id}
+                                SpecialistName={specialist.name}
+                                text={specialist.description}
+                                image={`/images/specialist${specialist.id}.png`}
+                                link={`/schedule/${specialist.id}`}
+                            />
+                        ))}
+                        {/* akhir item card specialist .....*/}
+                        {/* see more  */}
+                    </div>
                 </div>
+
                 <div className="w-full mt-5 flex md:p-3 ">
                     <button className="btn btn-sm mx-auto p-2">
                         See More ..
