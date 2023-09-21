4acd7acfdd4b92ac8d6e79ae2458becd84ed04e1
titik444 <35260719+titik444@users.noreply.github.com>
2023-09-21 15:12:35 +0700

    fix: typo min max

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,8 +36,8 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'nik' => 'required|numeric|max:10',
-            'phone' => 'required|numeric|min:10',
+            'nik' => 'required|numeric|min:10',
+            'phone' => 'required|numeric|min:8',
         ]);
 
         $user = User::create([
